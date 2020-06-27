de39eedeb7280cbebb70f2ee9637d15c43f4a130
Denis Vasilik <contact@denisvasilik.com>
2020-06-28 00:19:59 +0200

    Remove BufferedIODataProvider

--- binalyzer_data_provider/io.py
+++ binalyzer_data_provider/io.py
@@ -10,33 +10,3 @@
 """
 
 from binalyzer_core import DataProviderBase
-
-
-class BufferedIODataProvider(DataProviderBase):
-
-    def __init__(self, data):
-        self._data = data
-
-    @property
-    def data(self):
-        return self._data
-
-    @data.setter
-    def data(self, value):
-        self._data = value
-
-    def read(self, template):
-        """Read from the buffered IO :attr:`~binalyzer.binalyzer.BindingContext.stream`
-        using the position given by the :attr:`~binalyzer.binalyzer.BindingContext.template`.
-        """
-        absolute_address = template.absolute_address.value
-        size = template.size.value
-        self.data.seek(absolute_address)
-        return self.data.read(size)
-
-    def write(self, template, value):
-        """Write to the buffered IO :attr:`~binalyzer.binalyzer.BindingContext.stream`
-        using the position given by the :attr:`~binalyzer.binalyzer.BindingContext.template`.
-        """
-        self.data.seek(template.absolute_address.value)
-        self.data.write(value)
